518669166b9e0bddba03ead5a49c62973bf70310
saiteja051006 <mksaiteja705@gmail.com>
2026-04-01 09:48:19 +0530

    UC11

---
+++ App/src/UC11.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+import java.util.regex.*;
+
+public class UC11 {
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Train ID: ");
+        String trainId = sc.nextLine();
+
+        System.out.print("Enter Cargo Code: ");
+        String cargoCode = sc.nextLine();
+
+        Pattern trainPattern = Pattern.compile("TRN-\\d{4}");
+        Pattern cargoPattern = Pattern.compile("PET-[A-Z]{2}");
+
+        Matcher trainMatcher = trainPattern.matcher(trainId);
+        Matcher cargoMatcher = cargoPattern.matcher(cargoCode);
+
+        if (trainMatcher.matches()) {
+            System.out.println("Valid Train ID");
+        } else {
+            System.out.println("Invalid Train ID");
+        }
+
+        if (cargoMatcher.matches()) {
+            System.out.println("Valid Cargo Code");
+        } else {
+            System.out.println("Invalid Cargo Code");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
